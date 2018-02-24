68a2f38be0ee27fdfc5e50ce6098973e78fc4fe9
Vidir Alexander Jonsson <valex@valex.is>
2018-02-24 06:39:50 +0000

    Add LICENSE

--- lsftdi.c
+++ lsftdi.c
@@ -1,9 +1,26 @@
-/* lsftdi.c
+/*****************************************************************************/
 
-   Example for ftdi_usb_find_all()
+/*
+ *      lsftdi.c -- lspci like utility for FTDI USB devices
+ *
+ *      Copyright (C) 2003-2017
+ *        Intra2net AG and the libftdi developers (opensource@intra2net.com)
+ *      Copyright (C) 2018
+ *        Vidir Alexander Jonsson (valex@valex.is)
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ */
 
-   This program is distributed under the GPL, version 2
-*/
+/*****************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
